1bbca5b8708194d62f8eb8ff2c545a61e1421e75
Pitiphong Phongpattranont <pitiphong.p@me.com>
2012-11-19 23:05:14 +0700

    Use AFResourceIdentifierFromReferenceObject for reference the identifier in the backing context
    Thanks @conradev for the point.
    https://github.com/AFNetworking/AFIncrementalStore/pull/103#issuecomment-10517348

--- AFIncrementalStore/AFIncrementalStore.m
+++ AFIncrementalStore/AFIncrementalStore.m
@@ -806,7 +806,7 @@ inline NSString * AFResourceIdentifierFromReferenceObject(id referenceObject) {
                                              [[NSMutableSet alloc] initWithCapacity:[relationshipObject count]]);
             for (NSManagedObject *relationship in relationshipObject) {
                 NSManagedObjectID *backingRelationshipID = [self objectIDForBackingObjectForEntity:relationshipDescrption.entity
-                                                                            withResourceIdentifier:[self referenceObjectForObjectID:relationship.objectID]];
+                                                                            withResourceIdentifier:AFResourceIdentifierFromReferenceObject([self referenceObjectForObjectID:relationship.objectID])];
                 if (backingRelationshipID) {
                     NSManagedObject *backingRelationshipObject = [backingObject.managedObjectContext existingObjectWithID:backingRelationshipID
                                                                                                                     error:NULL];
@@ -819,7 +819,7 @@ inline NSString * AFResourceIdentifierFromReferenceObject(id referenceObject) {
             [backingObject setValue:backingRelationshipObjects forKey:relationshipName];
         } else {
             NSManagedObjectID *backingRelationshipID = [self objectIDForBackingObjectForEntity:relationshipDescrption.entity
-                                                                        withResourceIdentifier:[self referenceObjectForObjectID:[relationshipObject objectID]]];
+                                                                        withResourceIdentifier:AFResourceIdentifierFromReferenceObject([self referenceObjectForObjectID:[relationshipObject objectID]])];
             if (backingRelationshipID) {
                 NSManagedObject *backingRelationshipObject = [backingObject.managedObjectContext existingObjectWithID:backingRelationshipID
                                                                                                                 error:NULL];
