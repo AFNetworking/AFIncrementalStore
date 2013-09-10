92e2773105753124fe725eb3565e5b0c64d2170d
Joel Garrett <joel.garrett@willowtreeapps.com>
2013-09-10 15:29:31 -0400

    resolve bad access crash on newValuesForObjectWithID by referenceing the request operation

--- AFIncrementalStore/AFIncrementalStore.m
+++ AFIncrementalStore/AFIncrementalStore.m
@@ -800,6 +800,7 @@ withAttributeAndRelationshipValuesFromManagedObject:(NSManagedObject *)managedOb
                             NSManagedObjectContext *backingContext = [self backingManagedObjectContext];
                             [backingContext performBlockAndWait:^{
                                 AFSaveManagedObjectContextOrThrowInternalConsistencyException(backingContext);
+                                [operation isFinished];
                             }];
                         }];
                         
